c9f4e685144e6d055b0db32823a84925eec68ca3
RaGoose <daveyeinde@gmail.com>
2025-04-02 21:51:36 +0300

    final touch

--- main.go
+++ main.go
@@ -15,18 +15,13 @@ func main() {
 	if botToken == "" {
 		log.Fatal("TELEGRAM_BOT_TOKEN environment variable is not set")
 	}
+
 	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
 		log.Fatalf("Failed to create bot: %v", err)
 	}
 
-	go func() {
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("OK"))
-		})
-		log.Fatal(http.ListenAndServe(":8000", nil))
-	}()
+	go startHealthCheckServer()
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -37,26 +32,60 @@ func main() {
 
 	channelID := "@ragoosedumps"
 
-	for update := range updates {
-		if isValidMessage(strings.Trim(update.Message.Text, " ")) && !update.Message.IsCommand() {
-			msg := tgbotapi.NewMessageToChannel(channelID, update.Message.Text+"\n\n#frombot")
+	processUpdates(bot, updates, channelID)
+}
 
-			if _, err := bot.Send(msg); err != nil {
-				log.Printf("Failed to send message to channel: %v", err)
+func startHealthCheckServer() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func processUpdates(bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel, channelID string) {
+	for update := range updates {
+		if update.Message != nil {
+			if update.Message.IsCommand() {
+				handleCommand(bot, update.Message)
+				continue
+			}
+			if isValidMessage(strings.TrimSpace(update.Message.Text)) {
+				sendMessageToChannel(bot, channelID, update.Message.Text)
 			}
 		}
 	}
 }
 
-// rule for the message to be sent to the channel
+func handleCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	switch message.Command() {
+	case "start":
+		startMessage := `Welcome! This bot is inspired by Jack Rhysider's experiment, allowing you to post directly to @ragoose_dumps Telegram channel. 
+Simply send your message, and it will appear @ragoose_dumps with the hashtag #frombot. 
+Please note that links and files are not supported. for obvious reasons.`
+		msg := tgbotapi.NewMessage(message.Chat.ID, startMessage)
+		if _, err := bot.Send(msg); err != nil {
+			log.Printf("Failed to send start message: %v", err)
+		}
+	}
+}
+
+func sendMessageToChannel(bot *tgbotapi.BotAPI, channelID, text string) {
+	msg := tgbotapi.NewMessageToChannel(channelID, text+"\n\n#frombot")
+	if _, err := bot.Send(msg); err != nil {
+		log.Printf("Failed to send message to channel: %v", err)
+	}
+}
+
 func isValidMessage(msg string) bool {
 	if msg == "" {
 		return false
 	}
-	if len(msg) > 1 && msg[0] == '@' {
+	if strings.Contains(msg, "@") {
 		return false
 	}
-	urlRegex := `(?i)\b((?:https?://|www\.|)\S+\.\S+)`
+
+	urlRegex := `(?i)\b((?:https?|://|www\.|)\S+\.\S+)`
 	if matched, _ := regexp.MatchString(urlRegex, msg); matched {
 		return false
 	}
